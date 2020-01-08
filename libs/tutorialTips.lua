e94fa019c7142f073891999325243d0f81919176
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2020-01-08 18:57:20 +0100

    Update tutorialTips.lua
    Added caching of tips stored on disk to make functions calls faster.

--- libs/tutorialTips.lua
+++ libs/tutorialTips.lua
@@ -1,42 +1,88 @@
 
 ---------------------------------------------------------------------
--- Tutorial Tips v1.0 - code library
+-- Tutorial Tips v1.1 - code library
 ---------------------------------------------------------------------
 -- small helper lib to manage tutorial tips that will only display once per profile.
 -- can be reset, and would likely be done via a mod option.
---
--- id of the tip should preferably be somewhat unique to avoid collision with other mods,
--- since we will write data to Global_Texts along with everyone else.
 
-local this = {tips = {}}
+local mod = mod_loader.mods[modApi.currentMod]
+local this = {}
+local cachedTips
 
-function this:Add(id, tip)
-	assert(type(id) == 'string')
-	assert(type(tip) == 'table')
-	assert(type(tip.title) == 'string')
-	assert(type(tip.text) == 'string')
-	
-	self.tips[id] = true
-	Global_Texts[id .."_Title"] = tip.title
-	Global_Texts[id .."_Text"] = tip.text
+sdlext.config(
+	"modcontent.lua",
+	function(obj)
+		obj.tutorialTips = obj.tutorialTips or {}
+		obj.tutorialTips[mod.id] = obj.tutorialTips[mod.id] or {}
+		cachedTips = obj.tutorialTips
+	end
+)
+
+-- writes tutorial tips data.
+local function writeData(id, obj)
+	sdlext.config(
+		"modcontent.lua",
+		function(readObj)
+			readObj.tutorialTips[mod.id][id] = obj
+			cachedTips = readObj.tutorialTips
+		end
+	)
 end
 
-function this:Trigger(id, loc)
-	if not modApi:readProfileData(id) then
-		Game:AddTip(id, loc)
-		modApi:writeProfileData(id, true)
+-- reads tutorial tips data.
+local function readData(id)
+	local result = nil
+	
+	if cachedTips then
+		result = cachedTips[mod.id][id]
+	else
+		sdlext.config(
+			"modcontent.lua",
+			function(readObj)
+				cachedTips = readObj.tutorialTips
+				result = cachedTips[mod.id][id]
+			end
+		)
 	end
+	
+	return result
+end
+
+function this:ResetAll()
+	sdlext.config(
+		"modcontent.lua",
+		function(obj)
+			obj.tutorialTips = obj.tutorialTips or {}
+			obj.tutorialTips[mod.id] = {}
+			cachedTips = obj.tutorialTips
+		end
+	)
 end
 
 function this:Reset(id)
-	if not id then
-		for id, _ in pairs(self.tips) do
-			modApi:writeProfileData(id, false)
-		end
-	end
+	assert(type(id) == 'string')
+	writeData(id, nil)
+end
+
+function this:Add(tip)
+	assert(type(tip) == 'table')
+	assert(type(tip.id) == 'string')
+	assert(type(tip.title) == 'string')
+	assert(type(tip.text) == 'string')
 	
-	if self.tips[id] then
-		modApi:writeProfileData(id, false)
+	Global_Texts[mod.id .. tip.id .."_Title"] = tip.title
+	Global_Texts[mod.id .. tip.id .."_Text"] = tip.text
+end
+
+function this:Trigger(id, loc)
+	assert(type(id) == 'string')
+	assert(type(loc) == 'userdata')
+	assert(type(loc.x) == 'number')
+	assert(type(loc.y) == 'number')
+	
+	if not readData(id) then
+		Game:AddTip(mod.id .. id, loc)
+		writeData(id, true)
 	end
 end
 
